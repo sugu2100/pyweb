8014f30c77fc0970e25c66959175624a155f30fd
sugu2100 <sugu2100@daum.net>
2021-12-20 12:49:35 +0900

    pyweb detail

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from board import views
 
+app_name = 'board'  #네임 스페이스 설정
+
 urlpatterns = [
     #127.0.0.1:8000/board/
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail')
 ]
--- board/views.py
+++ board/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from board.models import Question
 
 def index(request):
-    season = '겨울'
+    question_list = Question.objects.all()  #db 전체조회
     return render(request, 'board/question_list.html',
-                  {'season':season})
+                  {'question_list':question_list})
     #return HttpResponse("pyweb 사이트 입니다.")
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id) #해당 id의 질문
+    return render(request, 'board/detail.html', {'question':question})
