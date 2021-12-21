8550dc4011b44677538cfda4eecf74d89a6e20df
sugu2100 <sugu2100@daum.net>
2021-12-21 10:48:43 +0900

    form upload

--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,12 @@ app_name = 'board'  #네임 스페이스 설정
 
 urlpatterns = [
     #127.0.0.1:8000/board/
+    #질문 목록
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail')
+    #질문/답변 상세
+    path('<int:question_id>/', views.detail, name='detail'),
+    #질문 등록
+    path('question/create/', views.question_create, name='question_create'),
+    #답변 등록
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
