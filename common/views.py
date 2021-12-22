050d2936730db33b96d1aa2f03dd6a63673759d1
sugu2100 <sugu2100@daum.net>
2021-12-22 12:29:17 +0900

    회원가입

--- common/views.py
+++ common/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from common.forms import UserForm
+
+def signup(request):
+    #회원 가입
+    if request.method == "POST":
+        form = UserForm(request.POST) #입력값을 가져옴
+        if form.is_valid():
+            form.save()  #실제 저장
+            return redirect('board:index')
+    else:
+        form = UserForm() #비어있는 새폼
+    return render(request, 'common/signup.html', {'form':form})
